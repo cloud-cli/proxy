2c5c195003a826010f792270662322b0f05d610e
Darlan Alves <me@darlanalv.es>
2023-09-04 12:41:07 +0000

    chore: add test scaffold

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,25 @@
+import { createServer } from 'node:http';
+import { ProxyServer } from '.';
+
+let port: number;
+let server: any;
+
+beforeAll(() => {
+  server && server.close();
+  server = createServer((req, res) => res.end(req.url))
+  port = 1000 + Math.floor(Math.random()*55000);
+  server.listen(port);
+});
+
+afterAll(() => {
+  server && server.close();
+});
+
 describe("ProxyServer", () => {
   it("should proxy an HTTP request", () => {
+    const server = new ProxyServer();
+    server.start();
+
     expect(1).toBe(1);
   });
 });
