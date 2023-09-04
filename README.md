5c746c4d0ef4c17e7f7ad5e09f774f8493bf9704
Darlan Alves <me@darlanalv.es>
2023-09-04 13:23:04 +0200

    feat: first version

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# ts-template
+# @cloud-cli/proxy
+
+HTTP(S) Proxy server
+
+## Settings
+
+**certificatesFolder:**
+
+The server automatically loads SSL certificates from a folder, where each subfolder is a domain name.
+Sub-domains are automatically resolved to their parent domain when requested.
+`sub.example.com` will use the certificate from `example.com` if available.
+
+For example, loads certs for `*.foo.com` and `*.example.com`:
+
+```js
+certificatesFolder = "/var/ssl";
+```
+
+And in that folder, have "foo.com" and "example.com" as subfolders.
+
+**certificateFile/keyFile:**
+
+Names of the files from where certificates are loaded. Defaults are:
+
+```js
+certificateFile = "fullchain.pem";
+keyFile = "privkey.pem";
+```
+
+**httpPort/httpsPort:**
+
+Allow you to change the http ports. Defaults are `80` and `443`;
+
+## API
+
+```ts
+import { ProxyServer, ProxySettings, ProxyEntry } from '@cloud-cli/proxy';
+
+const settings = new ProxySettings({
+  certificatesFolder: '/path/to/ssl',
+});
+
+const server = new ProxyServer(settings);
+
+server.start();
+
+server.add(new ProxyEntry({
+  domain: 'example.com',
+  redirectToDomain: 'www.example.com',
+}));
+
+server.add(new ProxyEntry({
+  domain: 'old.example.com',
+  redirectToUrl: 'https://www.example.com/',
+}));
+
+server.add(new ProxyEntry({
+  domain: 'www.example.com',
+  target: 'http://localhost:1234/',
+}));
+
+```
+
+## Environment variables
+
+| var | description |
+|-|-|
+| DEBUG | Enable debug logging |
+| PROXY_CERTS_FOLDER | Path to a folder where SSL certificates are stored |
--- src/index.ts
+++ src/index.ts
@@ -4,87 +4,156 @@ import {
   IncomingMessage,
   ClientRequest,
   ServerResponse,
-} from 'http';
-import { createServer as createHttpsServer, request as httpsRequest, ServerOptions as HttpsServerOptions } from 'https';
-import { createSecureContext, SecureContext } from 'tls';
+} from "http";
+import {
+  createServer as createHttpsServer,
+  request as httpsRequest,
+  ServerOptions as HttpsServerOptions,
+} from "https";
+
+import { createSecureContext, SecureContext } from "tls";
+import { readdir, readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { EventEmitter } from "node:events";
 
 export class ProxyEntry {
-  domain: string;
-  target: string;
-  redirectToHttps: boolean;
-  redirectToUrl: string;
-  cors: boolean;
+  readonly domain: string;
+  readonly target: string | URL;
+  readonly redirectToHttps: boolean = false;
+  readonly redirectToUrl: string = "";
+  readonly redirectToDomain: string = "";
+  readonly cors: boolean = false;
 
   constructor(p: Partial<ProxyEntry>) {
-    Object.assign(this,  p);
+    Object.assign(this, p);
   }
 }
 
-export class ProxyServer {
+export class ProxySettings {
+  readonly certificatesFolder = process.env.PROXY_CERTS_FOLDER;
+  readonly certificateFile = "fullchain.pem";
+  readonly keyFile = "privkey.pem";
+  readonly httpPort = 80;
+  readonly httpsPort = 443;
+
+  constructor(p: Partial<ProxySettings> = {}) {
+    Object.assign(this, p);
+  }
+}
+
+export class ProxyServer extends EventEmitter {
   protected certs: Record<string, SecureContext> = {};
-  protected targets: Record<string, ProxyEntry> = {};
-  protected servers: any = [];
+  protected proxies: Record<string, ProxyEntry> = {};
+  protected servers: Array<ReturnType<typeof createHttpServer>> = [];
+  protected settings: ProxySettings;
+  protected autoReload: any;
+
+  constructor(settings: ProxySettings = new ProxySettings()) {
+    super();
+    this.settings = settings;
+    this.reset();
+    const oneDay = 1000 * 60 * 60 * 24;
+    this.autoReload = setInterval(() => this.reload(), oneDay);
+  }
 
   start() {
+    this.reset();
     this.reload();
-    this.servers.forEach((server: any) => server.close());
 
     this.servers = [
-      createHttpServer((req, res) => this.serveRequest(req, res, true)).listen(80),
-      createHttpsServer(this.getSslOptions(), (req, res) => this.serveRequest(req, res)).listen(443),
+      createHttpServer((req, res) => this.serveRequest(req, res, true)).listen(
+        this.settings.httpPort
+      ),
+
+      createHttpsServer(this.getSslOptions(), (req, res) =>
+        this.serveRequest(req, res, false)
+      ).listen(this.settings.httpsPort),
     ];
+    return this;
+  }
+
+  reset() {
+    this.servers.forEach((server: any) => server.close());
+    this.proxies = {};
+    this.certs = {};
+    return this;
   }
 
   reload() {
     this.loadCertificates();
-    this.loadTargets();
+    return this;
+  }
+
+  add(proxy: ProxyEntry) {
+    this.proxies[proxy.domain] = proxy;
+    return this;
+  }
+
+  protected async loadCertificate(folder: string) {
+    const { certificatesFolder, certificateFile, keyFile } = this.settings;
+
+    if (process.env.DEBUG) {
+      console.log(`Loading certificates from ${certificatesFolder}/${folder}`);
+    }
+
+    createSecureContext({
+      cert: await readFile(
+        join(certificatesFolder, folder, certificateFile),
+        "utf8"
+      ),
+      key: await readFile(join(certificatesFolder, folder, keyFile), "utf8"),
+    });
   }
 
   protected async loadCertificates() {
     const certs = (this.certs = {});
 
-    const localCerts = await readdir(certificatesFolder, { withFileTypes: true });
-    const folders = localCerts.filter((entry) => entry.isDirectory()).map((dir) => dir.name);
+    const localCerts = await readdir(this.settings.certificatesFolder, {
+      withFileTypes: true,
+    });
+
+    const folders = localCerts
+      .filter((entry) => entry.isDirectory())
+      .map((dir) => dir.name);
 
     for (const rootDomain of folders) {
-      if (process.env.DEBUG) {
-        console.log(`Loading certificate for ${rootDomain}`);
-      }
-
-      certs[rootDomain] = createSecureContext({
-        cert: await readFile(join(certificatesFolder, rootDomain, certificateFile), 'utf8'),
-        key: await readFile(join(certificatesFolder, rootDomain, keyFile), 'utf8'),
-      });
+      certs[rootDomain] = this.loadCertificate(rootDomain);
     }
   }
 
-  protected async loadTargets() {
-    const targets = await Resource.find(ProxyEntry, new Query());
-    this.targets = {};
-    targets.forEach((entry) => (this.targets[entry.domain] = entry));
-  }
-
-  protected serveRequest(req: IncomingMessage, res: ServerResponse, insecure = false) {
-    const origin = this.getOrigin(req);
-    const proxyEntry = this.targets[origin?.hostname];
+  protected serveRequest(
+    req: IncomingMessage,
+    res: ServerResponse,
+    insecure = false
+  ) {
+    const origin = this.getRequestOrigin(req);
+    const proxyEntry = this.proxies[origin?.hostname];
 
     if (!origin.hostname || !proxyEntry) {
-      res.writeHead(404, 'Not found');
+      res.writeHead(404, "Not found");
       res.end();
       return;
     }
 
-    if (proxyEntry.redirectUrl) {
-      res.setHeader('Location', String(proxyEntry.redirectUrl));
-      res.writeHead(302, 'Moved somewhere else');
+    if (proxyEntry.redirectToDomain) {
+      const newURL = new URL(req.url, `https://${proxyEntry.redirectToDomain}`);
+      res.setHeader("Location", String(newURL));
+      res.writeHead(302, "Moved somewhere else");
       res.end();
       return;
     }
 
-    if (proxyEntry.redirect && insecure) {
+    if (proxyEntry.redirectToUrl) {
+      res.setHeader("Location", String(proxyEntry.redirectToUrl));
+      res.writeHead(302, "Moved somewhere else");
+      res.end();
+      return;
+    }
+
+    if (proxyEntry.redirectToHttps && insecure) {
       const newURL = new URL(req.url, `https://${req.headers.host}`);
-      res.setHeader('Location', String(newURL));
-      res.writeHead(301, 'HTTPS is better');
+      res.setHeader("Location", String(newURL));
+      res.writeHead(301, "HTTPS is better");
       res.end();
       return;
     }
@@ -92,37 +161,43 @@ export class ProxyServer {
     const target = proxyEntry.target;
     const url = new URL(req.url, target);
 
-    const isCorsPreflight = req.method === 'OPTIONS' && proxyEntry.cors;
+    const isCorsPreflight = req.method === "OPTIONS" && proxyEntry.cors;
     if (isCorsPreflight) {
       this.setCorsHeaders(req, res);
-      res.writeHead(204, { 'Content-Length': '0' });
+      res.writeHead(204, { "Content-Length": "0" });
       res.end();
       return;
     }
 
-    const proxyRequest = (url.protocol === 'https:' ? httpsRequest : httpRequest)(url, { method: req.method });
+    const proxyRequest = (
+      url.protocol === "https:" ? httpsRequest : httpRequest
+    )(url, { method: req.method });
     this.setHeaders(req, proxyRequest);
-    proxyRequest.setHeader('host', this.getHost(target));
-    proxyRequest.setHeader('x-forwarded-for', req.headers.host);
-    proxyRequest.setHeader('x-forwarded-proto', insecure ? 'http' : 'https');
-    proxyRequest.setHeader('forwarded', 'host=' + req.headers.host + ';proto=' + (insecure ? 'http' : 'https'));
+    proxyRequest.setHeader("host", this.getHostnameFromUrl(String(target)));
+    proxyRequest.setHeader("x-forwarded-for", req.headers.host);
+    proxyRequest.setHeader("x-forwarded-proto", insecure ? "http" : "https");
+    proxyRequest.setHeader(
+      "forwarded",
+      "host=" + req.headers.host + ";proto=" + (insecure ? "http" : "https")
+    );
 
-    req.on('data', (chunk) => proxyRequest.write(chunk));
-    req.on('end', () => proxyRequest.end());
+    req.on("data", (chunk) => proxyRequest.write(chunk));
+    req.on("end", () => proxyRequest.end());
 
-    proxyRequest.on('error', (error) => this.handleError(error, res));
-    proxyRequest.on('response', (proxyRes) => {
+    proxyRequest.on("error", (error) => this.handleError(error, res));
+    proxyRequest.on("response", (proxyRes) => {
       this.setHeaders(proxyRes, res);
 
-      const isCorsSimple = req.method !== 'OPTIONS' && proxyEntry.cors && req.headers.origin;
+      const isCorsSimple =
+        req.method !== "OPTIONS" && proxyEntry.cors && req.headers.origin;
       if (isCorsSimple) {
         this.setCorsHeaders(req, res);
       }
 
       res.writeHead(proxyRes.statusCode, proxyRes.statusMessage);
 
-      proxyRes.on('data', (chunk) => res.write(chunk));
-      proxyRes.on('end', () => res.end());
+      proxyRes.on("data", (chunk) => res.write(chunk));
+      proxyRes.on("end", () => res.end());
     });
   }
 
@@ -134,20 +209,21 @@ export class ProxyServer {
         const rootDomain = server.findRootDomain(domain);
 
         if (rootDomain) {
+          server.emit("sni", rootDomain);
           return cb(null, rootDomain);
         }
 
-        cb(new Error('Not found', { cause: 404 }), null);
+        cb(new Error("Not found"), null);
       },
     };
   }
 
   protected findRootDomain(domain: string) {
-    const parts = domain.split('/')[0].split('.');
+    const parts = domain.split("/")[0].split(".");
     const certs = this.certs;
 
     while (parts.length) {
-      const rootDomain = parts.join('.');
+      const rootDomain = parts.join(".");
 
       if (certs[rootDomain]) {
         return certs[rootDomain];
@@ -159,7 +235,10 @@ export class ProxyServer {
     return null;
   }
 
-  protected setHeaders(from: IncomingMessage, to: ServerResponse | ClientRequest) {
+  protected setHeaders(
+    from: IncomingMessage,
+    to: ServerResponse | ClientRequest
+  ) {
     const headers = Object.entries(from.headers);
 
     for (const header of headers) {
@@ -167,33 +246,35 @@ export class ProxyServer {
     }
   }
 
-  protected getOrigin(req: IncomingMessage): URL {
-    const host = req.headers['x-forwarded-for'] || req.headers.host || '';
-    return (host && new URL('http://' + host)) || null;
+  protected getRequestOrigin(req: IncomingMessage): URL {
+    const host = req.headers["x-forwarded-for"] || req.headers.host || "";
+    return (host && new URL("http://" + host)) || null;
   }
 
-  protected getHost(string: string) {
+  protected getHostnameFromUrl(string: string) {
     const url = new URL(string);
-    return url.hostname + (url.port ? ':' + url.port : '');
+    return url.hostname + (url.port ? ":" + url.port : "");
   }
 
   protected setCorsHeaders(req: IncomingMessage, res: ServerResponse) {
     const headers = req.headers;
     const corsOrigin = new URL(req.headers.origin).origin;
-    const allowedMethod = headers['access-control-request-method'] || 'GET,HEAD,PUT,PATCH,POST,DELETE';
-    const allowedHeaders = headers['access-control-request-headers'] || '*';
+    const allowedMethod =
+      headers["access-control-request-method"] ||
+      "GET,HEAD,PUT,PATCH,POST,DELETE";
+    const allowedHeaders = headers["access-control-request-headers"] || "*";
 
-    res.setHeader('Vary', 'Origin');
-    res.setHeader('Access-Control-Allow-Origin', corsOrigin);
-    res.setHeader('Access-Control-Allow-Headers', allowedHeaders);
-    res.setHeader('Access-Control-Allow-Methods', allowedMethod);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader("Vary", "Origin");
+    res.setHeader("Access-Control-Allow-Origin", corsOrigin);
+    res.setHeader("Access-Control-Allow-Headers", allowedHeaders);
+    res.setHeader("Access-Control-Allow-Methods", allowedMethod);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
   protected handleError(error: any, res: ServerResponse) {
-    console.error(error);
+    this.emit("error", error);
 
-    if (error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET') {
+    if (error.code === "ECONNREFUSED" || error.code === "ECONNRESET") {
       res.writeHead(502);
       res.end();
       return;
